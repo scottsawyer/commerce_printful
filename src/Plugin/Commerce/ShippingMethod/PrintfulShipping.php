c5fff5fcfd351e1895150ab0c67932a60518b509
Marcin Grabias <mgrabias@ogtrading.eu>
2019-10-31 15:24:35 +0100

    Finalized basic shipping rate logic.

--- src/Plugin/Commerce/ShippingMethod/PrintfulShipping.php
+++ src/Plugin/Commerce/ShippingMethod/PrintfulShipping.php
@@ -3,13 +3,17 @@
 namespace Drupal\commerce_printful\Plugin\Commerce\ShippingMethod;
 
 use Drupal\commerce_shipping\Plugin\Commerce\ShippingMethod\ShippingMethodBase;
+use Drupal\commerce_currency_resolver\Plugin\Commerce\CommerceCurrencyResolverAmountTrait;
 use Drupal\commerce_printful\Service\PrintfulInterface;
+use Psr\Log\LoggerInterface;
+use Drupal\commerce_shipping\ShippingService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\commerce_price\Price;
 use Drupal\commerce_shipping\Entity\ShipmentInterface;
 use Drupal\commerce_shipping\PackageTypeManagerInterface;
 use Drupal\commerce_shipping\ShippingRate;
-use Drupal\commerce_shipping\ShippingService;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\commerce_printful\Exception\PrintfulException;
 
 /**
  * Provides the FlatRate shipping method.
@@ -21,12 +25,23 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class PrintfulShipping extends ShippingMethodBase {
 
+  use CommerceCurrencyResolverAmountTrait {
+    buildConfigurationForm as public currencyBuildConfigurationForm;
+  }
+
   /**
    * The printful API service.
    *
    * @var \Drupal\commerce_printful\Service\PrintfulInterface
    */
   protected $pf;
+
+  /**
+   * Logger for this plugin.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
 
   /**
    * Constructor.
@@ -37,18 +52,25 @@ class PrintfulShipping extends ShippingMethodBase {
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
+   * @param \Drupal\commerce_shipping\PackageTypeManagerInterface $package_type_manager
+   *   Package type manager.
    * @param \Drupal\commerce_printful\Service\PrintfulInterface $pf
    *   The package type manager.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger factory service.
    */
   public function __construct(
     array $configuration,
     $plugin_id,
     $plugin_definition,
-    PrintfulInterface $pf
+    PackageTypeManagerInterface $package_type_manager,
+    PrintfulInterface $pf,
+    LoggerInterface $logger
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $package_type_manager);
 
     $this->pf = $pf;
+    $this->logger = $logger;
   }
 
   /**
@@ -59,23 +81,18 @@ class PrintfulShipping extends ShippingMethodBase {
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('commerce_printful.printful')
+      $container->get('plugin.manager.commerce_package_type'),
+      $container->get('commerce_printful.printful'),
+      $container->get('logger.factory')->get('commerce_printful')
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function defaultConfiguration() {
-    return [] + parent::defaultConfiguration();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildConfigurationForm($form, $form_state);
-
+    $form = $this->currencyBuildConfigurationForm($form, $form_state);
+    unset($form['default_package_type']);
     return $form;
   }
 
@@ -83,8 +100,6 @@ class PrintfulShipping extends ShippingMethodBase {
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    parent::submitConfigurationForm($form, $form_state);
-
     if (!$form_state->getErrors()) {
       $values = $form_state->getValue($form['#parents']);
     }
@@ -96,38 +111,74 @@ class PrintfulShipping extends ShippingMethodBase {
   public function calculateRates(ShipmentInterface $shipment) {
     // Rate IDs aren't used in a flat rate scenario because there's always a
     // single rate per plugin, and there's no support for purchasing rates.
+
+    if ($shipment->getShippingProfile()->get('address')->isEmpty()) {
+      return [];
+    }
+
+    $rates = [];
+
     $order = $shipment->getOrder();
-    kdpm($order);
+    $printful_items = [];
+    foreach ($shipment->getOrder()->getItems() as $orderItem) {
+      $purchasedEntity = $orderItem->getPurchasedEntity();
+      if (isset($purchasedEntity->printful_reference) && !empty($purchasedEntity->printful_reference->first()->printful_id)) {
+        $printful_items[] = [
+          'external_variant_id' => $purchasedEntity->printful_reference->first()->printful_id,
+          'quantity' => $orderItem->getQuantity(),
+          // TODO: We could include value here but docs don't specify currency,
+          // probably conversion to USD would be needed.
+        ];
+      }
+    }
 
-    /*
-    $items[0] = [
-      'external_variant_id' => '5da5cf680020c1',
-      'quantity' => 1,
-    ];
-    try {
-      $rates = $this->pf->shippingRates([
+    if (!empty($printful_items)) {
+      $address = $shipment->getShippingProfile()->get('address')->first()->getValue();
+      $request_data = [
         'recipient' => [
-          'address1' => '',
-          'city' => '',
-          'country_code' => '',
-          'zip' => '',
+          'address1' => $address['address_line1'],
+          'city' => $address['locality'],
+          'country_code' => $address['country_code'],
+          'state_code' => !empty($address['administrative_area']) ? $address['administrative_area'] : NULL,
+          'zip' => $address['postal_code'],
         ],
-        'items' => $items,
-      ]);
-    }
-    catch (PrintfulException $e) {
-      // Logger, whatever.
+        'items' => $printful_items,
+      ];
+
+      try {
+        $result = $this->pf->shippingRates($request_data);
+
+        foreach ($result['result'] as $printful_shipping_option) {
+          $price = new Price($printful_shipping_option['rate'], $printful_shipping_option['currency']);
+          $service = new ShippingService($printful_shipping_option['id'], $printful_shipping_option['name']);
+          $rates[$printful_shipping_option['rate']] = new ShippingRate($printful_shipping_option['id'], $service, $price);
+        }
+        // Sort by price ASC.
+        ksort($rates);
+      }
+      catch (PrintfulException $e) {
+        // TODO: Save request data to PrintfulException.
+        $this->logger->error(
+          "Couldn't load shipping data. Error: @error, input: @input",
+          [
+            '@error' => $e->getMessage(),
+            '@input' => json_encode($request_data),
+          ]
+        );
+      }
     }
 
-    foreach ($rates as $key => $rate) {
-      $price  = new Price($rate['price_including_gst'], 'NZD');
-      $availableRates[$rate['price_including_gst']] = new ShippingRate($key,  $this->services[$key], $price);
-    }
-    // Sort by price ASC.
-    ksort($availableRates);
-    return $availableRates;
-    */
-    return [];
+    return $rates;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function selectRate(ShipmentInterface $shipment, ShippingRate $rate) {
+    // Plugins can override this method to store additional information
+    // on the shipment when the rate is selected (for example, the rate ID).
+    $shipment->setShippingService($rate->getService()->getId());
+    $shipment->setAmount($rate->getAmount());
   }
 
 }
